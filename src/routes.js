51f261f4cb674f4995451209adb7217b82da94a1
yeremi777 <kurikarakenjou@gmail.com>
2021-05-05 03:18:44 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,36 @@
+const {
+    addBookHandler,
+    getAllBooksHandler,
+    getBooksByIdHandler,
+    editBookByIdHandler,
+    deleteBookByIdHandler
+} = require("./handler");
+
+const routes = [{
+        method: 'POST',
+        path: '/books',
+        handler: addBookHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books',
+        handler: getAllBooksHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books/{id}',
+        handler: getBooksByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/books/{id}',
+        handler: editBookByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/books/{id}',
+        handler: deleteBookByIdHandler,
+    },
+];
+
+module.exports = routes;
